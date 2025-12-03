ae3a485d857dad78984643b336e4c1c30209df95
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2025-12-03 14:07:37 +0530

    Convert Km to Miles and Viceversa

---
+++ problem6.py
@@ -0,0 +1,15 @@
+# Convert KM to MILES
+# INPUT
+print("---------- Km -> Miles -----------")
+km = float(input("Enter Kilometers : "))
+Miles = km * 1.61
+print("Miles = ",Miles)
+print()
+
+print("---------- Miles -> Km -----------")
+miles = float(input("Enter Miles : "))
+Km = miles/1.61
+print("Kilometers = ",Km)
+
+# OUTPUT
+# Based on user input
