7fe418734425f3ca625223ddfb42b6615ab661a4
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2025-12-03 14:35:42 +0530

    Checking Leap Year

---
+++ problem9.py
@@ -0,0 +1,10 @@
+# Check LEAP Year
+
+# INPUT
+year = int(input("Enter the year in the format (YYYY) : "))
+if year % 400 == 0 and year % 100 == 0:
+    print("Leap Year")
+elif year % 4 == 0 and year % 100 != 0:
+    print("Leap Year")
+else:
+    print("Not a Leap Year")
