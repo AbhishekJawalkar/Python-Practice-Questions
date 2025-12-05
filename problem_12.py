900a007e5417a45ef071d9271fc97efe9af1473e
AbhishekJawalkar <abhishekjawalkar111@gmail.com>
2025-12-05 11:28:35 +0530

    Generating random number

---
+++ problem_12.py
@@ -0,0 +1,9 @@
+# Generating random number
+
+import random
+
+a = random.randint(1,100) # Gives random number from the range given 
+print(f"The random generated number is : {a}")
+
+b = random.random() # Gives random Float number
+print(b)
